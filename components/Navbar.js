b7fcc77348c73c504a3fb62252b7c0ab2e64df7e
shanewmurphy <shn.murphy@gmail.com>
2022-10-25 10:58:49 +0100

    First Commit Next-UI Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,89 @@
+import { Dropdown, Grid, Image } from "@nextui-org/react";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
+export default function Nav() {
+  return (
+    <div>
+      <div className={styles.outer_navbar_container}>
+        <div className={styles.inner_navbar}>
+          <div className={styles.brand}>
+            <Link href="/">
+              <a>
+                <Image
+                  src="/logo.svg"
+                  height={28}
+                  width={103}
+                  alt="Logo image"
+                />
+              </a>
+            </Link>
+          </div>
+          <div className={styles.nav_links_container}>
+            <ul>
+              <li>
+                <Link href="/about">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Dropdown flat>
+                  <Dropdown.Button
+                    flat
+                    css={{
+                      background: "transparent",
+                      color: "white",
+                      height: "$11",
+                      borderRadius: "0",
+                      boxShadow: "0",
+                      fontSize: "16px",
+                      fontWeight: 500,
+                      "&:hover": {
+                        background: "transparent",
+                        color: "$white",
+                      },
+                      "&:active": {
+                        background: "transparent",
+                        color: "$white",
+                      },
+                      "&:focus": {
+                        borderColor: "transparent",
+                        background: "transparent",
+                        color: "$white",
+                      },
+                    }}
+                  >
+                    Roofs
+                  </Dropdown.Button>
+                  <Dropdown.Menu css={{ $$dropdownMenuWidth: "180px" }}>
+                    <Dropdown.Item>
+                      <Link href="/about">Pitch Roofs</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link href="/about">Flat Roofs</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link href="/about">Slate Roofs</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link href="/about">Conservatory</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link href="/about">Roofs Repairs</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link href="/about">Fibreglass Roofs</Link>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li>Ground Works</li>
+              <li>Why Choose Us</li>
+            </ul>
+          </div>
+          <div className={styles.cta_call}>Call 085 256 257</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+import { NextUIProvider, createTheme } from "@nextui-org/react";
+import Navbar from "../components/Navbar";
+const theme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      backgroundColor: "223b40",
+      primary: "#223b40",
+      green: "#223b40",
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextUIProvider theme={theme}>
+        <Navbar />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
+  );
+}
+
+export default MyApp;
